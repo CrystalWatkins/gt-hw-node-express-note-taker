f6bb83391d058ff7451dbfc1c4ee53e08b230cab
Crystal <crystalwatkins428@gmail.com>
2020-07-05 16:41:46 -0400

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // CONFIRM WITH LINE 12-14 goes here for data-parse POST
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 app.get("/notes", (req, res) => {
@@ -27,24 +27,24 @@ app.get("/api/notes", (req, res) => {
     });
 });
 
-app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/quotes.json", "utf8", (err, data) => {
-        if (err) {
-            return res.send("An error occurred reading your data");
-        }
-        const arrayOfNotes = JSON.parse(data);
-        arrayOfNotes.push(req.body);
-        fs.writeFile("./db/db.json",
-        JSON.stringify(arrayOfNotes), "utf8", 
-        (err) => {
-            if (err) {
-                return res.send("An error occurred writing your data");
-            }
-            res.json(arrayOfNotes);
-        }
-        );
-    });
-});
+// app.post("/api/notes", (req, res) => {
+//     fs.readFile("./db/quotes.json", "utf8", (err, data) => {
+//         if (err) {
+//             return res.send("An error occurred reading your data");
+//         }
+//         const arrayOfNotes = JSON.parse(data);
+//         arrayOfNotes.push(req.body);
+//         fs.writeFile("./db/db.json",
+//         JSON.stringify(arrayOfNotes), "utf8", 
+//         (err) => {
+//             if (err) {
+//                 return res.send("An error occurred writing your data");
+//             }
+//             res.json(arrayOfNotes);
+//         }
+//         );
+//     });
+// });
 
 // listen to that port
 app.listen(PORT, () => {
